bc22e4e24f27d315cf61a5368e92b016b0871b1d
Stephen Johnson <steve@steveasleep.com>
2013-01-20 22:03:33 -0800

    Javascript wankery

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+var getHash = function() {
+  var match = window.location.href.match(/#(.*)$/);
+  if (match) {
+    return match[1];
+  } else {
+    return null
+  }
+}
+
+var update = function() {
+  var hash = getHash();
+
+  if (hash) {
+    $('.content *').hide();
+    var $title = $('#' + hash);
+    $title.show()
+    $title.nextUntil('h1').show()
+  } else {
+    window.location.replace("#issue-1");
+    update();
+  }
+}
+
 $(document).ready(function() {
   var $newToc = $('<div class="toc"></div>').html($('.toc').html());
   $('.toc').remove();
   $newToc.appendTo('body');
+
+  $('li').click(update);
+  update();
 });
